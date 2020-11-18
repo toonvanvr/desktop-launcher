0c9cae478453cff8be442f08ba65b7e8ecb3f704
AVCADO <devavcado@gmail.com>
2020-11-18 17:55:56 -0600

    started the icon

---
+++ src/window/desktop/Icon.vue
@@ -0,0 +1,23 @@
+<!-- I have no idea what I'm doing -->
+<template>
+  <div id="icon">
+    <p v-if="image.legnth > 0"><img v-bind:src="icon" /></p>
+    <button @click="showIcon()">Show the SVG!</button>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+// Declare tha variable
+export default {
+  data: function() {
+    return {
+      icon: ''
+    }
+  },
+  methods: {
+    showIcon: function() {
+      this.icon = './' /* Stuck here, how do i do this? */
+    }
+  }
+}
+</script>
