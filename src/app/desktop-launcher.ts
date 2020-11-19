932390014ca39ff8247ee64b8ed3d18ed874375e
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2020-11-19 20:57:41 +0100

    add a tray icon to 'quit' and 'toggle devtools'

--- src/app/desktop-launcher.ts
+++ src/app/desktop-launcher.ts
@@ -1,42 +1,66 @@
-import { BrowserWindow } from 'electron'
+import { BrowserWindow, Menu, Tray } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import { MODE } from './helpers'
+import { join } from 'path'
 
 export class DesktopLauncherApplication {
+  tray: Tray | null = null
   desktop: BrowserWindow | null = null
 
   async launch(): Promise<void> {
-    await this.createDesktopWindow()
+    await Promise.all([this.createTray(), this.createDesktopWindow()])
   }
 
-  async createDesktopWindow(): Promise<void> {
+  async createDesktopWindow(): Promise<BrowserWindow> {
     // Allow only one instance
-    if (this.desktop) return
+    if (!this.desktop) {
+      this.desktop = new BrowserWindow({
+        width: 500,
+        height: 500,
+        transparent: true,
+        frame: false,
+        webPreferences: {
+          // Configure me in vue.config.js
+          nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+        }
+      })
 
-    // Create a window
-    this.desktop = new BrowserWindow({
-      width: 500,
-      height: 500,
-      transparent: true,
-      frame: false,
-      webPreferences: {
-        // Use pluginOptions.nodeIntegration, leave this alone
-        // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-        nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+      // Load the main page
+      if (process.env.WEBPACK_DEV_SERVER_URL) {
+        // Development: with live reload
+        await this.desktop.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+      } else {
+        // production: static local file access
+        createProtocol('app')
+        await this.desktop.loadURL('app://./index.html')
       }
-    })
-
-    // Load the main page
-    if (process.env.WEBPACK_DEV_SERVER_URL) {
-      // development
-      await this.desktop.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-      if (!MODE.testing) {
-        // this.mainWindow.webContents.openDevTools({ mode: 'detach' })
-      }
-    } else {
-      // production
-      createProtocol('app')
-      this.desktop.loadURL('app://./index.html')
     }
+    return this.desktop
+  }
+
+  async createTray(): Promise<Tray> {
+    // TODO: referring to `src` is not allowed, but let's do it like this for now
+    this.tray = new Tray(join(__dirname, '../src/assets/tray.png'))
+    this.tray.setContextMenu(
+      Menu.buildFromTemplate([
+        { type: 'separator' },
+        {
+          label: 'Layers',
+          submenu: [
+            {
+              label: 'Desktop',
+              submenu: [
+                {
+                  role: 'toggleDevTools',
+                  click: () =>
+                    this.desktop?.webContents.openDevTools({ mode: 'detach' })
+                }
+              ]
+            }
+          ]
+        },
+        { role: 'quit' }
+      ])
+    )
+    return this.tray
   }
 }
